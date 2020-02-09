5221d1659219346763537295f6b40d4152bfd50b
Bastian <Pohl>
2020-02-09 21:31:43 +0100

    exit after fetchAndStore

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 // ESM syntax is supported.
-// integrate .env files
-import * as dotenv from "dotenv";
 
+// integrate env files
+import * as dotenv from "dotenv";
 dotenv.config()
 
 // import local functions
 import { Fetcher } from "./lib/controller/fints_fetcher";
 
-
+// fetch transactions and store to sql db 
 const fetch = async () => {
   await Fetcher.fetchAndStore()
+  process.exit()
 }
 
+// run
 fetch()
